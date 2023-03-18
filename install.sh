02b3f54e8de47e20bec3b1a6be66f25b4bcf0f02
M6GSN <48696854+M6GSN@users.noreply.github.com>
2023-03-18 13:46:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ udo apt update && sudo apt full-upgrade -y
 sudo apt install gpsd-clients -y
 wget https://github.com/la5nta/pat/releases/download/v0.13.1/pat_0.13.1_linux_armhf.deb 
 sudo dpkg -i pat_0.13.1_linux_armhf.deb -y
+pat configure
